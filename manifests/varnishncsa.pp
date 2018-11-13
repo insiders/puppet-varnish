55977f0af899cf2b7ee87d1da1d876fded557cbe
Martijn Heemels <martijn@insiders.nl>
2018-11-13 18:15:50 +0100

    Comments

--- manifests/varnishncsa.pp
+++ manifests/varnishncsa.pp
@@ -15,8 +15,10 @@ class varnish::varnishncsa (
 
   if $::varnish::params::service_provider == 'systemd' {
 
+    # Escape the logformat for the systemd unit file
     $_logformat = regsubst($logformat, '%', '%%', 'G')
 
+    # Create a directory and drop-in unit file
     file { '/etc/systemd/system/varnishncsa.service.d/':
       ensure => 'directory',
     }
@@ -28,6 +30,7 @@ class varnish::varnishncsa (
       notify  => Exec['varnishncsa_systemctl_daemon_reload'],
     }
 
+    # Reload systemd units
     exec { 'varnishncsa_systemctl_daemon_reload':
       command     => '/bin/systemctl daemon-reload',
       refreshonly => true,
@@ -37,8 +40,10 @@ class varnish::varnishncsa (
 
   } elsif $::varnish::params::service_provider == 'sysvinit' {
 
+    # Escape the logformat for the sysvinit file
     $_logformat = regsubst($logformat, '"', '\\\"', 'G')
 
+    # Varnishncsa is disabled by default. Override that, putting Puppet in charge
     file { '/etc/default/varnishncsa':
       owner   => 'root',
       group   => 'root',
@@ -47,6 +52,7 @@ class varnish::varnishncsa (
       notify  => Service['varnishncsa'],
     }
 
+    # Add the logformat to the sysvinit script
     file {'/etc/init.d/varnishncsa':
       owner   => 'root',
       group   => 'root',
