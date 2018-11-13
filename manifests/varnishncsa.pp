d02611b2b022fa094ed671441d3a65ce1039ebf5
Martijn Heemels <martijn@insiders.nl>
2018-11-13 15:58:48 +0100

    Override the varnishncsa logformat via a systemd drop-in unit file.

--- manifests/varnishncsa.pp
+++ manifests/varnishncsa.pp
@@ -13,15 +13,25 @@ class varnish::varnishncsa (
     enable => $enabled,
   }
 
-  notify { 'logformat':
-    message => $logformat,
-  }
+  if $::varnish::params::service_provider == 'systemd' {
 
-  file { '/etc/systemd/system/varnishncsa.service.d/':
-    ensure => 'directory',
-  }
-  file { '/etc/systemd/system/varnishncsa.service.d/varnishncsa.conf':
-    ensure  => 'present',
-    content => "[Service]\nExecStart=\nExecStart=/usr/bin/varnishncsa -a -w /var/log/varnish/varnishncsa.log -D -F${logformat}\n"
+    file { '/etc/systemd/system/varnishncsa.service.d/':
+      ensure => 'directory',
+    }
+
+    file { '/etc/systemd/system/varnishncsa.service.d/varnishncsa.conf':
+      ensure  => 'present',
+      content => "[Service]\nExecStart=\nExecStart=/usr/bin/varnishncsa -a -w /var/log/varnish/varnishncsa.log -D -F ${logformat}\n",
+      require => File['/etc/systemd/system/varnishncsa.service.d/'],
+      notify  => Exec['varnishncsa_systemctl_daemon_reload'],
+    }
+
+    exec { 'varnishncsa_systemctl_daemon_reload':
+      command     => '/bin/systemctl daemon-reload',
+      refreshonly => true,
+      require     => File['/etc/systemd/system/varnishncsa.service.d/varnishncsa.conf'],
+      notify      => Service['varnishncsa'],
+    }
+
   }
 }
